af1043f3888717386acaae8fee1c0638d20e60aa
mmapas <tartazapas@gmail.com>
2021-01-24 09:59:24 +0100

    subir

---
+++ Practica4CW/src/main/java/com/org/Practica4CW/Practica4CW/App.java
@@ -0,0 +1,13 @@
+package com.org.Practica4CW.Practica4CW;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Practica4CW/src/test/java/com/org/Practica4CW/Practica4CW/PruebaTest.java
@@ -0,0 +1,12 @@
+package com.org.Practica4CW.Practica4CW;
+
+import org.junit.Test;
+
+public class PruebaTest {
+	
+	@Test
+	public void imprimir() {
+		System.out.println("Practica 4 CW por Miguel marin pascual");
+		
+	}
+}
